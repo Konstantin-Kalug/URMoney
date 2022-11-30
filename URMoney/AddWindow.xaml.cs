a0235184278c48992d097eedbd69fe41a25b3fa3
Konstantin_Kalug <nl17nl17@outlook.com>
2022-11-30 21:38:51 +0700

    добавлен интерфейс для работы с базой данных

---
+++ URMoney/AddWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace URMoney
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public string[] output = new string[6];
+        public AddWindow(List<Dictionary<int, string>> dicts, DataItem? dataItem)
+        {
+            InitializeComponent();
+            InitializeComboBoxes(dicts, dataItem);
+        }
+        private void InitializeComboBoxes(List<Dictionary<int, string>> dicts, DataItem dataItem)
+        {
+            foreach (var elem in dicts[0])
+                categoryBox.Items.Add(elem.Value);
+            foreach (var elem in dicts[1])
+                peopleBox.Items.Add(elem.Value);
+            foreach (var elem in dicts[2])
+                transactionBox.Items.Add(elem.Value);
+            foreach (var elem in dicts[3])
+                valuteBox.Items.Add(elem.Value);
+            if (dataItem is null) return;
+            categoryBox.SelectedItem = dataItem.Column2;
+            transactionBox.SelectedItem = dataItem.Column3;
+            peopleBox.SelectedItem = dataItem.Column4;
+            valuteBox.SelectedItem = dataItem.Column6;
+            dateBox.Text = dataItem.Column7.ToString();
+            noteBox.Text = dataItem.Column8;
+        }
+        void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            output = new string[] { categoryBox.Text, peopleBox.Text, transactionBox.Text, valuteBox.Text, dateBox.Text, noteBox.Text};
+            DialogResult = true;
+        }
+    }
+}
--- URMoney/MainWindow.xaml.cs
+++ URMoney/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,134 @@ namespace URMoney
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ApplicationContext db = new ApplicationContext();
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+        // при загрузке окна
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // гарантируем, что база данных создана
+            db.Database.EnsureCreated();
+            // загружаем данные из БД
+            db.Operations.Load();
+            // и устанавливаем данные в качестве контекста
+            DataContext = db.Operations.Local.ToObservableCollection();
+            InitializeBD();
+            OutputDataGrid();
+        }
+        // первоначальное добавление элементов, если их нет (без учета модуля валют)
+        private void InitializeBD()
+        {
+            // обязательный человек, не изменяется
+            if (db.Peoples.ToArray().Length == 0)
+            {
+                db.Peoples.Add(new People { Name = "Неизвестно" });
+            }
+            // обязательные 2 типа, не изменяются
+            if (db.Types.ToArray().Length == 0)
+            {
+                db.Types.Add(new Type { Title = "Доходы" });
+                db.Types.Add(new Type { Title = "Расходы" });
+            }
+            if (db.Categories.ToArray().Length == 0)
+            {
+                // вставить дефолтные категории
+            }
+            if (db.Valutes.ToArray().Length == 0)
+            {
+                // вставить работу с request
+            }
+            db.SaveChanges();
+        }
+        private List<Dictionary<int, string>> InitializeKeysBD()
+        {
+            List<Dictionary<int, string>> dicts = new List<Dictionary<int, string>>();
+            dicts.Add(new Dictionary<int, string>());
+            foreach (var elem in db.Categories.ToArray())
+                if (elem.TypeId == 2) dicts[0].Add(elem.Id, elem.Title);
+            dicts.Add(new Dictionary<int, string>());
+            foreach (var elem in db.Peoples.ToArray())
+                dicts[1].Add(elem.Id, elem.Name);
+            dicts.Add(new Dictionary<int, string>());
+            foreach (var elem in db.Transactions.ToArray())
+                dicts[2].Add(elem.Id, elem.Title);
+            dicts.Add(new Dictionary<int, string>());
+            foreach (var elem in db.Valutes.ToArray())
+                dicts[3].Add(elem.Id, elem.Title);
+            return dicts;
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            List<Dictionary<int, string>> keys = InitializeKeysBD();
+            AddWindow AddWindow = new AddWindow(keys, null);
+            if (AddWindow.ShowDialog() == true)
+            {
+                try
+                {
+                    Operation Operation = new Operation { CategoryId = db.Categories.Where(p => p.Title == AddWindow.output[0]).ToArray()[0].Id, PeopleId = db.Peoples.Where(p => p.Name == AddWindow.output[1]).ToArray()[0].Id, TransactionId = db.Transactions.Where(p => p.Title == AddWindow.output[2]).ToArray()[0].Id, ValuteId = db.Valutes.Where(p => p.Title == AddWindow.output[3]).ToArray()[0].Id, Date = DateOnly.Parse(AddWindow.output[4]), Note = AddWindow.output[5] };
+                    db.Operations.Add(Operation);
+                    db.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Пожалуйства заполните все поля корректными данными!");
+                }
+                OutputDataGrid();
+            }
+        }
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            // получаем выделенный объект
+            DataItem? dataItem = table.SelectedItem as DataItem;
+            // если ни одного объекта не выделено, выходим
+            if (dataItem is null) return;
+            List<Dictionary<int, string>> keys = InitializeKeysBD();
+            AddWindow AddWindow = new AddWindow(keys, dataItem);
+            if (AddWindow.ShowDialog() == true)
+            {
+                try
+                {
+                    // получаем измененный объект
+                    Operation operation = db.Operations.Find(dataItem.Column1);
+                    if (operation != null)
+                    {
+                        operation.CategoryId = db.Categories.Where(p => p.Title == AddWindow.output[0]).ToArray()[0].Id;
+                        operation.PeopleId = db.Peoples.Where(p => p.Name == AddWindow.output[1]).ToArray()[0].Id;
+                        operation.TransactionId = db.Transactions.Where(p => p.Title == AddWindow.output[2]).ToArray()[0].Id;
+                        operation.ValuteId = db.Valutes.Where(p => p.Title == AddWindow.output[3]).ToArray()[0].Id;
+                        operation.Date = DateOnly.Parse(AddWindow.output[4]);
+                        operation.Note = AddWindow.output[5];
+                        db.SaveChanges();
+                        OutputDataGrid();
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Пожалуйства заполните все поля корректными данными!");
+                }
+            }
+        }
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            // получаем выделенный объект
+            DataItem? dataItem = table.SelectedItem as DataItem;
+            // если ни одного объекта не выделено, выходим
+            if (dataItem is null) return;
+            Operation operation = db.Operations.Find(dataItem.Column1);
+            db.Operations.Remove(operation);
+            db.SaveChanges();
+            OutputDataGrid();
+        }
+        private void OutputDataGrid()
+        {
+            Operation[] elems = db.Operations.ToArray();
+            List<DataItem> items = new List<DataItem>();
+            foreach (var elem in elems)
+                items.Add(new DataItem { Column1 = elem.Id, Column2 = db.Categories.Find(elem.CategoryId).Title, Column3 = db.Transactions.Find(elem.TransactionId).Title, Column4 = db.Peoples.Find(elem.PeopleId).Name, Column5 = db.Transactions.Find(elem.TransactionId).Total, Column6 = db.Valutes.Find(elem.ValuteId).Title, Column7 = elem.Date, Column8 = elem.Note });
+            table.ItemsSource = items;
         }
         // Таблицы Учёта
         private void Button_Click_2(object sender, RoutedEventArgs e)
